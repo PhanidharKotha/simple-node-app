79d801a54e74640b614a0bbbf9fdf375846eb626
PhanidharKotha <kotha.phanidhar@email.com>
2025-07-25 21:46:56 -0500

    simple node js app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from your Node.js app!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
